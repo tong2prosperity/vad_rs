9584599bf0ee1c051243fb0fb2213599ef354a17
liutong <liutong@msh.team>
2024-09-26 22:37:34 +0800

    add vad basic lib

--- src/exposure/mod.rs
+++ src/exposure/mod.rs
@@ -1,41 +1,37 @@
 pub mod model;
-use crate::ort_vad::silero;
-use crate::ort_vad::utils::SampleRate;
-
-use jni::objects::{JObject, JValue};
-use jni::JNIEnv;
-use jni::objects::{JByteArray, JClass};
-use jni::sys::{jfloat, jlong};
 use crate::ort_vad::silero::Silero;
-use std::sync::Mutex;
+use crate::ort_vad::utils::SampleRate;
+use std::sync::{Mutex, Arc};
 
 lazy_static::lazy_static! {
-    static ref SILERO_INSTANCE: Mutex<Option<Silero>> = Mutex::new(None);
+    pub static ref SILERO_INSTANCE: Arc<Mutex<Option<Silero>>> = Arc::new(Mutex::new(None));
 }
 
-#[no_mangle]
-pub extern "system" fn Java_com_example_Vad_initSilero(env: JNIEnv) -> jlong {
+// 移除了JNI相关的函数
+
+pub fn init_silero() -> *mut Arc<Mutex<Option<Silero>>> {
     let silero = Silero::new(SampleRate::SixteenkHz, "").unwrap();
     let mut instance = SILERO_INSTANCE.lock().unwrap();
     *instance = Some(silero);
-    Box::into_raw(Box::new(instance)) as jlong
+    Box::into_raw(Box::new(SILERO_INSTANCE.clone()))
 }
 
-#[no_mangle]
-pub extern "system" fn Java_com_example_Vad_processAudio<'a>(env: JNIEnv<'a>, _: JClass, audio_data: JByteArray<'a>) -> jfloat {
-    let audio_bytes: Vec<u8> = env.convert_byte_array(&audio_data).unwrap();
-    let audio_i16: Vec<i16> = audio_bytes.chunks_exact(2)
-        .map(|chunk| i16::from_le_bytes([chunk[0], chunk[1]]))
-        .collect();
+pub fn process_audio(audio_i16: &[i16]) -> f32 {
+    if audio_i16.is_empty() {
+        return -1.0;
+    }
+    if audio_i16.len() <  {
+        return -1.0;
+    }
+
 
     let mut instance = SILERO_INSTANCE.lock().unwrap();
     
     if let Some(ref mut silero) = *instance {
-        match silero.calc_level(&audio_i16) {
+        match silero.calc_level(audio_i16) {
             Ok(level) => {
-                // 根据 level 判断是否包含语音
-                if level > 0.01 { // 设定一个阈值
-                    return 1.0; // 包含语音
+                if level > 0.01 {
+                    return 1.0;
                 }
             }
             Err(_) => {
@@ -43,5 +39,5 @@ pub extern "system" fn Java_com_example_Vad_processAudio<'a>(env: JNIEnv<'a>, _:
             }
         }
     }
-    0.0 // 不包含语音
+    0.0
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,25 @@ pub mod exposure;
 pub mod ort_vad;
 pub mod util;
 
+use jni::objects::{JByteArray, JClass};
+use jni::JNIEnv;
+use jni::sys::{jfloat, jlong};
+use crate::exposure::{init_silero, process_audio};
 
+#[no_mangle]
+pub extern "system" fn Java_com_example_Vad_initSilero(_env: JNIEnv) -> jlong {
+    init_silero() as jlong
+}
 
+#[no_mangle]
+pub extern "system" fn Java_com_example_Vad_processAudio<'a>(env: JNIEnv<'a>, _: JClass, audio_data: JByteArray<'a>) -> jfloat {
+    let audio_bytes: Vec<u8> = env.convert_byte_array(&audio_data).unwrap();
+    let audio_i16: Vec<i16> = audio_bytes.chunks_exact(2)
+        .map(|chunk| i16::from_le_bytes([chunk[0], chunk[1]]))
+        .collect();
+
+    process_audio(&audio_i16) as jfloat
+}
 
 #[cfg(test)]
 mod tests {
