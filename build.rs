7005e466c563397123bba589c8e416f123721a2b
liutong <liutong@msh.team>
2024-11-25 13:34:15 +0800

    all ok

--- build.rs
+++ build.rs
@@ -10,19 +10,19 @@ fn main() {
     println!("cargo::warning=target_arch: {}", target_arch);
 
     // 设置库文件路径
-    // let lib_path = match (target_os.as_str(), target_arch.as_str()) {
-    //     ("macos", _) => "deps/darwin",
-    //     ("ios", "aarch64") => "deps/ios",
-    //     ("ios", "aarch64") => "deps/ios_sim",  // iOS 模拟器
-    //     _ => panic!("Unsupported platform: {} {}", target_os, target_arch),
-    // };
+    let lib_path = match (target_os.as_str(), target_arch.as_str()) {
+        ("macos", _) => "deps/darwin",
+        ("ios", "aarch64") => "deps/ios",
+        ("ios-sim", "aarch64") => "deps/ios_sim",  // iOS 模拟器
+        _ => panic!("Unsupported platform: {} {}", target_os, target_arch),
+    };
 
-    //println!("cargo::rustc-env=IPHONEOS_DEPLOYMENT_TARGET=13.0");
+    
 
     // 设置链接路径和库
     let current_dir = std::env::current_dir().unwrap();
-    println!("cargo::warning=current_dir: {}", current_dir.display());
-    //println!("cargo::rustc-link-search={}", current_dir.join(lib_path).display());
+    
+    
 
     // println!("cargo:rustc-link-arg=-framework");
     // println!("cargo:rustc-link-arg=CoreFoundation");
@@ -31,10 +31,13 @@ fn main() {
     // println!("cargo:rustc-link-arg=-framework");
     // println!("cargo:rustc-link-arg=Accelerate");
     if target_os == "ios" {
-        println!("cargo::rustc-link-lib=static=onnxruntime");
-        println!("cargo:rustc-link-arg=-fapple-link-rtlib");
-        println!("cargo:rustc-link-arg=-framework");
-        println!("cargo:rustc-link-arg=CoreML");
+        println!("cargo::warning=current_dir: {}", current_dir.display());
+        println!("cargo::rustc-link-search={}", current_dir.join(lib_path).display());
+        println!("cargo::rustc-env=IPHONEOS_DEPLOYMENT_TARGET=13.0");
+        //println!("cargo::rustc-link-lib=static=onnxruntime");
+//        println!("cargo::rustc-link-arg=-fapple-link-rtlib");
+        println!("cargo::rustc-link-arg=-framework");
+        println!("cargo::rustc-link-arg=CoreML");
     }
 
     if target_os == "android" {
